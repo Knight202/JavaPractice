9f0870bc81f6349b5d655b3f1c9a1410fef7b6a1
Rook@WorkPC <white905rook@gmail.com>
2013-07-22 00:04:18 -0400

    Sample contribution

--- src/Collection/ArrayCollection.java
+++ src/Collection/ArrayCollection.java
@@ -7,6 +7,7 @@ public class ArrayCollection {
 	public static void main(String[] args) {
 		Hashtable<Integer, String> AL = new Hashtable<Integer, String>(10);
 		
+//add next level code		
 		AL.put(1, "Kalpan");
 		AL.put(2, "Jatan");
 
